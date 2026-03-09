59da38f0b92c4a3a928b3b70a6adcd7190b6c01b
UltralyticsAssistant <web@ultralytics.com>
2026-03-09 04:13:22 +0000

    Auto-format by https://ultralytics.com/actions

--- ultralytics/nn/modules/SCI/multi_read_data.py
+++ ultralytics/nn/modules/SCI/multi_read_data.py
@@ -1,11 +1,11 @@
+import os
+import random
+
 import numpy as np
 import torch
 import torch.utils.data
-import random
-from PIL import Image
-from glob import glob
 import torchvision.transforms as transforms
-import os
+from PIL import Image
 
 batch_w = 600
 batch_h = 400
@@ -29,7 +29,7 @@ class MemoryFriendlyLoader(torch.utils.data.Dataset):
         self.transform = transforms.Compose(transform_list)
 
     def load_images_transform(self, file):
-        im = Image.open(file).convert('RGB')
+        im = Image.open(file).convert("RGB")
         img_norm = self.transform(im).numpy()
         img_norm = np.transpose(img_norm, (1, 2, 0))
         return img_norm
@@ -41,8 +41,8 @@ class MemoryFriendlyLoader(torch.utils.data.Dataset):
         h = low.shape[0]
         w = low.shape[1]
         #
-        h_offset = random.randint(0, max(0, h - batch_h - 1))
-        w_offset = random.randint(0, max(0, w - batch_w - 1))
+        random.randint(0, max(0, h - batch_h - 1))
+        random.randint(0, max(0, w - batch_w - 1))
         #
         # if self.task != 'test':
         #     low = low[h_offset:h_offset + batch_h, w_offset:w_offset + batch_w]
@@ -50,7 +50,7 @@ class MemoryFriendlyLoader(torch.utils.data.Dataset):
         low = np.asarray(low, dtype=np.float32)
         low = np.transpose(low[:, :, :], (2, 0, 1))
 
-        img_name = self.train_low_data_names[index].split('\\')[-1]
+        img_name = self.train_low_data_names[index].split("\\")[-1]
         # if self.task == 'test':
         #     # img_name = self.train_low_data_names[index].split('\\')[-1]
         #     return torch.from_numpy(low), img_name
